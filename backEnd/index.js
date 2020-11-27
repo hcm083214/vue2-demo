e6d23520f8ade7714316e36552fc6b0990025cb9
hcm083214 <13723742351@163.com>
2020-11-27 14:37:21 +0800

    a

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,17 +1,24 @@
 /*
  * @Author: 黄灿民
  * @Date: 2020-10-26 15:44:10
- * @LastEditTime: 2020-10-26 15:54:16
+ * @LastEditTime: 2020-11-27 14:37:10
  * @LastEditors: 黄灿民
  * @Description: 
- * @FilePath: \backEnd\index.js
+ * @FilePath: \00.test\backEnd\index.js
  */
 const Koa = require('koa');
 const Router = require('koa-router');
+const Koabody = require('koa-body')
 
 const app = new Koa();
 const router = new Router();
 
+app.use(Koabody());
+
 router.get('/test',ctx=>{
     ctx.body = '111';
 })
+router.post('/upload',ctx=>{
+    console.log(ctx.request.body)
+})
+app.listen(8080)
