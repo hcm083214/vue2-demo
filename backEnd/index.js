50ca234b835f32785d601f78438cc7dc29898dee
hcm083214 <13723742351@163.com>
2020-12-01 19:39:35 +0800

    a

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,20 +1,28 @@
 /*
  * @Author: 黄灿民
  * @Date: 2020-10-26 15:44:10
- * @LastEditTime: 2020-12-01 17:26:02
+ * @LastEditTime: 2020-12-01 19:05:05
  * @LastEditors: 黄灿民
  * @Description: 
  * @FilePath: \00.test\backEnd\index.js
  */
 const Koa = require('koa');
 const Router = require('koa-router');
+const koabody = require('koa-body')
 
 const app = new Koa();
 const router = new Router();
 
+app.use(koabody({
+    multipart:true
+}))
+
+
 router.post('/upload',ctx=>{
-    ctx.set('Access-Control-Allow-Origin',"*");
+    console.log(ctx.files)
     ctx.body = '111';
+    ctx.set("Access-Control-Allow-Origin", " * ");
 })
 
+app.use(router.routes())
 app.listen(3000);
