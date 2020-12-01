e4243957a363f046ee10c608c74d15fc05897c73
hcm083214 <13723742351@163.com>
2020-12-01 17:26:19 +0800

    a

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: 黄灿民
  * @Date: 2020-10-26 15:44:10
- * @LastEditTime: 2020-10-26 15:54:16
+ * @LastEditTime: 2020-12-01 17:26:02
  * @LastEditors: 黄灿民
  * @Description: 
- * @FilePath: \backEnd\index.js
+ * @FilePath: \00.test\backEnd\index.js
  */
 const Koa = require('koa');
 const Router = require('koa-router');
@@ -12,6 +12,9 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-router.get('/test',ctx=>{
+router.post('/upload',ctx=>{
+    ctx.set('Access-Control-Allow-Origin',"*");
     ctx.body = '111';
 })
+
+app.listen(3000);
